926ab7543cabe56456b4e671b89c17f7fb24815a
Amon <amonguinan@arizona.edu>
2023-11-04 13:57:24 -0700

    added model/model.py. excluded dataset due to size issues

---
+++ model/model.py
@@ -0,0 +1,11 @@
+#general plan from https://www.tensorflow.org/tutorials/images/classification
+
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL #use Pillow instead?
+import tensorflow as tf
+
+from tensorflow import keras
+from keras import layers, models
+from keras.models import Sequential
+
